ee3d2248dc0a4e653421f365a47370ccf2f5f17a
Karthik Ragunath A <a.karthikragunath@gmail.com>
2018-02-14 01:31:01 +0530

    White and Black Images Displayed

--- TextGrouping.py
+++ TextGrouping.py
@@ -10,3 +10,17 @@ numberOfColumns = len(copiedImage[0])
 print(numberOfRows)
 print(numberOfColumns)
 numpyImageArray = np.array(copiedImage)
+print("Numpy array row length is %d." %(len(numpyImageArray)))
+print("Numpy array column length is %d." %(len(numpyImageArray[0])))
+
+newImageListBlack = [[0 for x in range(len(numpyImageArray[0]))] for x in range(len(numpyImageArray))]
+newImageNumpyArrayBlack = np.array(newImageListBlack)
+#print(newImageListBlack)
+cv2.imwrite('BlackImage.jpg', newImageNumpyArrayBlack)
+cv2.imshow("Black Image", newImageNumpyArrayBlack)
+
+newImageListWhite = [[255 for x in range(len(numpyImageArray[0]))] for x in range(len(numpyImageArray))]
+newImageNumpyArrayWhite = np.array(newImageListWhite)
+#print(newImageNumpyArrayWhite)
+cv2.imwrite('WhiteImage.jpg', newImageNumpyArrayWhite)
+cv2.imshow("White Image", newImageNumpyArrayWhite)
